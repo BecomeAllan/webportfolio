e9b6f531fa07d2f5ad0b981eeb24959a60f16cac
BecomeAllan <57200740+BecomeAllan@users.noreply.github.com>
2021-07-07 10:04:48 +0000

    Deploying to gh-pages from @ BecomeAllan/webportfolio@121b19a44f967111b1a40e2104d904f53eb881d7 🚀

--- _next/static/7nltEYrLUkM_K1An6xUWG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8880bf90816f5a130940.js"],"/_error":["static/chunks/pages/_error-157366ffea25803f02fd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
