45a7b888c35bfdcca152e6a071be40ef701f40af
Allan Victor <allanvictor.almeida@gmail.com>
2021-07-15 01:52:12 -0300

    animation topbar

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,31 +1,42 @@
 import Link from 'next/link'
+// import { useState } from 'react';
+import { useRouter } from 'next/router'
+import React from 'react';
+// import { useLocation, useRouteMatch } from 'react-router-dom';
 
 import styles from './styles.module.scss'
 
+
 export function Topbar() {
+  const router = useRouter();
+
+  console.log(router.pathname);
+
   return (
     <div className={styles.container}>
       <ul>
         <li>
           <Link href="/">
-          <a >
-            HOME
-          <div className={styles.selected} />
-          </a>
+            <a>
+              HOME
+              <div className={router.pathname === "/" ? styles.selected : styles.notSelected} />
+            </a>
           </Link>
         </li>
         <li>
-        <Link href="/project">
-          <a>
-            PROJECT
-          </a>
-        </Link>
+          <Link href="/project">
+            <a>
+              PROJECT
+              <div className={router.pathname.includes("/project") ? styles.selected : styles.notSelected} />
+            </a>
+          </Link>
         </li>
         <li>
-          <Link href="about">
-          <a>
-            ABOUT
-          </a>
+          <Link href="/about">
+            <a>
+              ABOUT
+              <div className={router.pathname.includes("/about") ? styles.selected : styles.notSelected} />
+            </a>
           </Link>
         </li>
       </ul>
